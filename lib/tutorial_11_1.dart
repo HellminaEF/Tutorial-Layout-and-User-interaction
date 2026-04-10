04b8e39e669b92c6df81dfa16678c6c169dc781c
Hellmina Enjelina Fitri <hellminae@gmail.com>
2026-04-10 12:05:27 +0700

    tambah file tutorial_11_1.dart berisi PageView

---
+++ lib/tutorial_11_1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Tutorial11Page extends StatelessWidget {
+  const Tutorial11Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Tutorial 11-1', style: TextStyle(fontSize: 24)),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'Go to Home page',
+                    style: TextStyle(fontSize: 16, color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
